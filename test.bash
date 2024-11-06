9737d1dc587877fb143bdee54c47a8e1fa8207e7
Shizen Kotake <s23c1049pa@s.chibakoudai.jp>
2024-11-06 09:59:25 +0900

    test

--- test.bash
+++ test.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2024 Shizen Kotake
-#SPDX-License-Identifier: BSD-3-Clause
+# SPDX-License-Identifier: BSD-3-Clause
 ng () {
 	echo ${1}行目が違うよ
 	res=1
@@ -8,8 +8,18 @@ ng () {
 
 res=0
 
+### NORMAL INPUT ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+### STRANGE INPUT ###
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
